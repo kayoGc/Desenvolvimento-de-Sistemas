bdb76a28d8520ef23bfeca3e60c58643e586dc0d
kayoGc <kayivicti@gmail.com>
2023-04-11 22:16:39 -0300

    templete banco

---
+++ aula-11-04-2023/Banco-infos/src/Model/Banco_DAO.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Banco_DAO {
+
+    public static String nome, end, email;
+
+    public static void Juntainfo() {
+
+    }
+}
---
+++ aula-11-04-2023/Banco-infos/src/Model/Menu_DAO.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Menu_DAO {
+
+    public static void dados() {
+        Model.Banco_DAO.Juntainfo();
+    }
+
+    public static void info() {
+        Model.Banco_DAO.Juntainfo();
+    }
+
+    public static void resp() {
+        Model.Banco_DAO.Juntainfo();
+    }
+}
+ 
