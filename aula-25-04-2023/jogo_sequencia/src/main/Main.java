eec75d2e0544f7745c85b295b79f0bd2e93479aa
kayoGc <kayivicti@gmail.com>
2023-04-25 22:03:42 -0300

    aula java vetores

---
+++ aula-25-04-2023/jogo_sequencia/src/main/Main.java
@@ -0,0 +1,12 @@
+
+package main;
+
+import Model.jogo_DAO;
+
+public class Main {
+    public static void main(String[] args) {
+      jogo_DAO.sequencia();
+        
+    }
+    
+}
---
+++ aula-25-04-2023/vetores/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import Model.lista_DAO;
+import Model.random_DAO;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        random_DAO.geraAle();
+    }
+    
+}
