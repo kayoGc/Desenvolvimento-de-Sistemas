d1cf388f4b8c8e8bbf321617764b39b4c7bb3078
kayoGc <kayivicti@gmail.com>
2023-05-09 22:18:49 -0300

    opa

---
+++ aula-05-09-2023/Interface/src/main/Main.java
@@ -0,0 +1,15 @@
+
+package main;
+
+import View.Inicio_GUI;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        new Inicio_GUI().setVisible(true);
+        
+    }
+    
+}
